73dd7293f74dd6640a1053021c3304dae26a683c
luisCermeno <luis.cermeno.f@gmail.com>
2020-05-10 01:25:41 -0700

    "updated js"

--- cookieClicker/script.js
+++ cookieClicker/script.js
@@ -5,4 +5,9 @@ var counter = document.getElementById("counter");
 moon.onclick = function(){
   clicks++;
   counter.innerHTML = clicks;
+  if(moon.className == "shrinked"){
+      moon.className == ""
+  } else{
+        moon.className = "shrinked"
+  }
 }
